1183feca66b347434f23b43e2a6ed5e101e6ca3b
SevenJun <haojun0001@126.com>
2021-04-12 14:01:06 +0800

    重新提交

--- util.py
+++ util.py
@@ -10,7 +10,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
 from config import global_config
-from jd_logger import logger
 
 
 USER_AGENTS = ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
--- zq_spider_requests.py
+++ zq_spider_requests.py
@@ -14,6 +14,7 @@ from logger import logger
 from config import global_config
 from exception import SKException
 import js2xml
+from selenium import webdriver
 
 
 class ZqSpider(object):
@@ -21,6 +22,7 @@ class ZqSpider(object):
         self.in_file_path = global_config.getRaw('config', 'in_file_path')
         self.out_file_path = global_config.getRaw('config', 'out_file_path')
         self.spider_url = global_config.getRaw('config', 'spider_url')
+        self.spider_oz_url = global_config.getRaw('config', 'spider_oz_url')
         self.user_agent = global_config.getRaw('config', 'DEFAULT_USER_AGENT')
 
     def get_duiju(self):
@@ -32,14 +34,15 @@ class ZqSpider(object):
         resp.encoding = 'GBK'
         resp.encoding = 'utf-8'
 
-        logger.info('打开模板文件:' + self.in_file_path)
-        rb = xlrd.open_workbook(self.in_file_path,formatting_info=True)
+        logger.info('打开模板文件:' + os.getcwd() + self.in_file_path)
+        rb = xlrd.open_workbook(os.getcwd() + '\\' + self.in_file_path,formatting_info=True)
         wb = copy(rb)
         sheet = wb.get_sheet(rb.nsheets - 1)
 
         logger.info('开始解析联赛积分排名')
         soup = BeautifulSoup(resp.text,'lxml')
-        table_first = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > table')
+
+        table_first = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(1) td:nth-child(1) table')
 
         trs = table_first[0].find_all('tr')
         i = 0
@@ -60,8 +63,7 @@ class ZqSpider(object):
                 rows = rows + 1
             i = i + 1
         
-
-        table_second = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > table')
+        table_second = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(1) td:nth-child(2) table')
         trs = table_second[0].find_all('tr')
         i = 0
         cols = 11
@@ -83,7 +85,7 @@ class ZqSpider(object):
         
         ban_rows = rows
         logger.info('开始解析' + first_match_name + '半场')
-        table_third = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(2) > td:nth-child(1) > table')
+        table_third = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(2) td:nth-child(1) table')
         trs = table_third[0].find_all('tr')
         cols = 0
         for tr in trs:#遍历后续节点
@@ -94,7 +96,7 @@ class ZqSpider(object):
             rows = rows + 1
 
         logger.info('开始解析' + second_match_name + '半场')
-        table_fourth = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > table')
+        table_fourth = soup.select('#porlet_5 > div > table > tbody > tr:nth-child(2) td:nth-child(2) table')
         trs = table_fourth[0].find_all('tr')
         cols = 11
         rows = ban_rows
@@ -104,24 +106,31 @@ class ZqSpider(object):
                 sheet.write(rows,cols,td.text)
                 cols = cols + 1
             rows = rows + 1
+        
+        logger.info('开始爬取:' + self.spider_oz_url)
+        logger.info('开始解析欧指赔率')
 
+        driver = webdriver.PhantomJS()
+        driver.get(self.spider_oz_url)
+        html_oz = driver.execute_script('return document.documentElement.outerHTML')
+        soup_oz = BeautifulSoup(html_oz)
 
-        wb.save(self.out_file_path)
+        table_oz = soup_oz.select('#oddsList_tab')
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-        src = soup.select('#webmain script')[0].string
-        # print('---------')
-        # print(src)
-        src_element = js2xml.parse(src, encoding='utf-8', debug=False)
-        # print('---------')
-        print(src_element)
-        src_tree = js2xml.pretty_print(src_element)
-        print('---------')
-        print(src_tree)
-        print('---------')
-        number = src_element.xpath("//program/var[@name='v_data']")
+        trs_oz = table_oz[0].find_all('tr')
+        i = 0
+        cols = 0
+        rows = 78
+        first_match_name = ""
+        for tr in trs_oz:#遍历后续节点
+            cols = 0
+            for td in tr.find_all('td'):
+                sheet.write(rows,cols,td.text)
+                cols = cols + 1
+            rows = rows + 1
+            i = i + 1
 
-
-        logger.info('爬取完成:' + self.out_file_path)
+        wb.save(os.getcwd() + '\\' + self.out_file_path)
+        logger.info('爬取完成:' + os.getcwd() + self.out_file_path)
 
 
